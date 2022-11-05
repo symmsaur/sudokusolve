04d9c1464f9f76d835808106e7fd1012cae8c1bd
Jonathan Karlsson <karlsjona@gmail.com>
2022-11-05 22:49:54 +0100

    Use a bitmap for the possible values in the cells
    Speed up by 3x.

--- src/cell.rs
+++ src/cell.rs
@@ -1,25 +1,27 @@
+use bitmaps::Bitmap;
+
 #[derive(Debug)]
 pub struct EliminationError {}
 
 #[derive(Clone)]
 pub struct Cell {
-    possibles: Vec<i32>,
+    possibles: Bitmap<9>,
 }
 
 impl Cell {
     pub fn new() -> Cell {
         Cell {
-            possibles: (1..10).collect(),
+            possibles: Bitmap::mask(9)
         }
     }
 
     pub fn set_hint(self: &mut Cell, hint: i32) {
-        self.possibles.clear();
-        self.possibles.push(hint);
+        self.possibles = Bitmap::mask(0);
+        self.possibles.set((hint - 1) as usize, true);
     }
 
     pub fn eliminate_possible(self: &mut Cell, digit: i32) -> Result<(), EliminationError> {
-        self.possibles.retain(|i| *i != digit);
+        self.possibles.set((digit - 1) as usize, false);
         if self.possibles.len() == 0 {
             Err(EliminationError {})
         } else {
@@ -32,18 +34,18 @@ impl Cell {
     }
 
     pub fn first_possible(&self) -> Option<i32> {
-        self.possibles.first().copied()
+        self.possibles.first_index().map(|i| (i + 1) as i32)
     }
 
     pub fn possibles_except(&self, except: i32) -> Vec<i32> {
         self.possibles
-            .iter()
-            .cloned()
-            .filter(|x| *x != except)
+            .into_iter()
+            .filter(|x| *x != (except - 1) as usize)
+            .map(|i| (i + 1) as i32)
             .collect()
     }
 
     pub fn is_possible(&self, digit: i32) -> bool {
-        self.possibles.contains(&digit)
+        self.possibles.get(digit as usize)
     }
 }
