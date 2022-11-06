33998ff8919eb376239bb9f9bd53f082bdc825c8
Jonathan Karlsson <karlsjona@gmail.com>
2022-11-06 11:37:59 +0100

    Set reasonable delay in observer

--- src/observer.rs
+++ src/observer.rs
@@ -154,7 +154,7 @@ impl GridObserver for TermObserver {
             print!("{}", color::Fg(color::Black));
         }
         flush();
-        thread::sleep(time::Duration::from_millis(1));
+        thread::sleep(time::Duration::from_millis(20));
     }
     fn clear_cell(&self, cell_x: i32, cell_y: i32, cell: &Cell) {
         if cell.num_possibles() == 1 {
